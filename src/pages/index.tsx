2a958828bfd94b1d2d3e5b5515d7b69e0191c223
ryota8128 <ryota2339@icloud.com>
2023-07-08 17:50:57 +0900

    refactor:accountディレクトリで階層構造にした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import FormAddAccount from '@/components/account/FormAddAccount';
-import { Button } from 'reactstrap';
-import styles from '@/styles/Home.module.css';
-import Link from 'next/link';
 import { LinkButton } from '@/components/common/LinkButton';
 
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <>
       <main style={{ marginTop: 100 }}>
-        <LinkButton href='/addAccount' text='アカウント追加' />
+        <LinkButton href='/account/add' text='アカウント追加' />
         <LinkButton href='/login' text='ログイン' />
         <LinkButton href='/accountList' text='口座一覧' />
       </main>
