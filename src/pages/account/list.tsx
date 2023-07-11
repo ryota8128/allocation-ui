81682abb66194b65511666439add264352705f3b
ryota8128 <ryota2339@icloud.com>
2023-07-11 23:25:48 +0900

    feat: 全口座リストをテーブルで表示

--- src/pages/account/list.tsx
+++ src/pages/account/list.tsx
@@ -1,15 +1,37 @@
-import { NextPage } from 'next'
+import TableAccountList from '@/components/account/TableAccountLIst';
+import Account from '@/types/Account';
+import axios from 'axios';
+import { GetServerSideProps, NextPage } from 'next';
+import { getSession, useSession } from 'next-auth/react';
+import { headers } from 'next/dist/client/components/headers';
+import { Table } from 'reactstrap';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  const token = session?.accessToken;
+  const res = await axios.get('http://localhost:8080/api/account/list', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const accountList: Account[] = res.data;
+  console.log(accountList);
+
+  return {
+    props: { accountList },
+  };
+};
 
 interface Props {
-
+  accountList: Account[];
 }
 
-const list: NextPage<Props> = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const list: NextPage<Props> = ({ accountList }) => {
+  return (
+    <div>
+      <TableAccountList accountList={accountList} />
+    </div>
+  );
+};
 
-export default list
+export default list;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import { LinkButton } from '@/components/common/LinkButton';
-
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
   return (
     <>
       <main style={{ marginTop: 100 }}>
         <LinkButton href="/account/add" text="アカウント追加" />
         <LinkButton href="/login" text="ログイン" />
         <LinkButton href="/account/list" text="口座一覧" />
+        <p>{session?.user.name}</p>
+        <p>{session?.accessToken}</p>
       </main>
     </>
   );
