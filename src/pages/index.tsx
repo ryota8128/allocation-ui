650cf56224bf29a239191c3bc6393259427c01e3
ryota8128 <61615829+ryota8128@users.noreply.github.com>
2023-07-13 00:06:02 +0900

    feat: 認証付き口座登録 (#13)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <>
       <main style={{ marginTop: 100 }}>
-        <LinkButton href="/account/add" text="アカウント追加" />
         <LinkButton href="/login" text="ログイン" />
         <LinkButton href="/account/list" text="口座一覧" />
+        <LinkButton href="/account/add" text="口座追加" />
       </main>
     </>
   );
