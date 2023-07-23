27835db1288f8f8dfd10733cf0adc29ebeba54f8
ryota8128 <ryota2339@icloud.com>
2023-07-23 15:55:36 +0900

    feat: temporary, regular/ find追加

--- src/lib/temporaryReq.ts
+++ src/lib/temporaryReq.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const apiUrl = process.env.API_URL;
-export const getAccount = async (token: string) => {
+export const findTemporary = async (token: string) => {
   const res = await axios.get(`${apiUrl}/api/temporary/list`, {
     headers: {
       Authorization: `Bearer ${token}`,
