e26b4434f11cce460e0adab05286da8c5ce18e41
ryota8128 <ryota2339@icloud.com>
2023-07-23 12:39:41 +0900

    feat: Home実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,46 @@
 import AppTitle from '@/components/home/AppTitle';
 import Item from '@/components/home/Item';
-import { NextPage } from 'next';
+import { getTransfers } from '@/lib/transferReq';
+import Transfer from '@/types/Transfer';
+import { GetServerSideProps, NextPage } from 'next';
+import { getSession } from 'next-auth/react';
 
-interface Props {}
+interface Props {
+  transferList: Transfer[];
+}
 
-const Index: NextPage<Props> = () => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  const token = session?.accessToken;
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+  const transferList: Transfer[] = await getTransfers(token);
+
+  // const transferList = [{ id: 1, title: '202306', userId: 2 }];
+  return {
+    props: { transferList },
+  };
+};
+
+const Index: NextPage<Props> = ({ transferList }) => {
   return (
     <>
       <div className="container">
         <AppTitle>Money Allocation App</AppTitle>
-        <Item>新規振替</Item>
+        <Item href="/new-transfer">新規振替</Item>
+        {transferList.map((transfer) => (
+          <>
+            <Item key={transfer.id} href={`/transfer/[${transfer.id}]`}>
+              {transfer.title}
+            </Item>
+          </>
+        ))}
       </div>
     </>
   );
