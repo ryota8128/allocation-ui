182aeeea7031ed201ef0a683705913ef8c08c4cb
ryota8128 <ryota2339@icloud.com>
2023-07-27 02:36:40 +0900

    feat: raguler, temporary, update

--- src/components/transfer/TemporaryTransferTable.tsx
+++ src/components/transfer/TemporaryTransferTable.tsx
@@ -1,6 +1,8 @@
 import { NextPage } from 'next';
 import { Table } from 'reactstrap';
 import AccountDropdown from './AccountDropdown';
+import { CSSProperties, useState } from 'react';
+import { updateTemporary } from '@/lib/temporaryReq';
 
 interface Props {
   temporaryList: TemporaryTransfer[];
@@ -11,10 +13,56 @@ const TemporaryTransferTable: NextPage<Props> = ({
   temporaryList,
   accountList,
 }) => {
-  const updateTemporary = () => {};
+  const [defaultTemporaryList, setDefaultTemporaryList] =
+    useState<TemporaryTransfer[]>(temporaryList);
+  const [updatedTemporaryList, setUpdatedTemporaryList] =
+    useState<TemporaryTransfer[]>(temporaryList);
+
+  const inputStyle: CSSProperties = {
+    border: 'none',
+    outline: 'none',
+  };
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    changeId: number
+  ) => {
+    const property = e.target.name as keyof TemporaryTransfer;
+
+    // 表示してるtemporaryListを更新 and 変更のあるregularをisChange:true
+    setUpdatedTemporaryList(
+      updatedTemporaryList.map((temp) => {
+        if (temp.id !== changeId) {
+          return temp;
+        }
+
+        const defaultTemp = defaultTemporaryList.find(
+          (t) => t.id === changeId
+        ) as TemporaryTransfer;
+
+        const isChanged = e.target.value != defaultTemp[property];
+
+        // 変更後の値をセット
+        return {
+          ...temp,
+          [property]: e.target.value,
+          isChanged: isChanged,
+        };
+      })
+    );
+  };
+
+  const onBlur = (focusTemporary: TemporaryTransfer) => {
+    if (focusTemporary.isChanged) {
+      updateTemporary(focusTemporary);
+      setDefaultTemporaryList([...updatedTemporaryList]);
+      focusTemporary.isChanged = false;
+    }
+  };
+
   return (
     <div>
-      <Table hover>
+      <Table>
         <thead>
           <tr>
             <th>from</th>
@@ -24,7 +72,7 @@ const TemporaryTransferTable: NextPage<Props> = ({
           </tr>
         </thead>
         <tbody>
-          {temporaryList.map((temporary) => (
+          {updatedTemporaryList.map((temporary) => (
             <tr key={temporary.id}>
               <td>
                 <AccountDropdown
@@ -40,8 +88,26 @@ const TemporaryTransferTable: NextPage<Props> = ({
                   column="toAccount"
                 />
               </td>
-              <td>{temporary.description}</td>
-              <td>{temporary.amount}</td>
+              <td>
+                <input
+                  type="text"
+                  style={{ ...inputStyle, width: 150 }}
+                  name="description"
+                  value={temporary.description}
+                  onChange={(e) => onChange(e, temporary.id)}
+                  onBlur={() => onBlur(temporary)}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  style={{ ...inputStyle, width: 100 }}
+                  name="amount"
+                  value={temporary.amount}
+                  onChange={(e) => onChange(e, temporary.id)}
+                  onBlur={() => onBlur(temporary)}
+                />
+              </td>
             </tr>
           ))}
         </tbody>
