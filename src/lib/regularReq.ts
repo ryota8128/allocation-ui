733f19c30004b3f672997af3e8206f7875684616
ryota8128 <ryota2339@icloud.com>
2023-07-28 02:14:13 +0900

    feat: regularReq/insert実装

--- src/lib/regularReq.ts
+++ src/lib/regularReq.ts
@@ -1,5 +1,5 @@
+import RegularTransferTable from '@/components/transfer/RegularTransferTable';
 import axios from 'axios';
-import { headers } from 'next/dist/client/components/headers';
 
 const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
@@ -20,13 +20,27 @@ export const findRegular = async (token: string) => {
   return regularListEx;
 };
 
-export const updateRegular = async (
-  regular: RegularTransfer
-) => {
+export const updateRegular = async (regular: RegularTransfer) => {
   try {
     await axios.post('/api/regular/update', regular);
     console.log('Success to update RegularTransfer');
   } catch (error) {
     console.log('Failed to update RegularTransfer');
+    // TODO: ログインし直し
+  }
+};
+
+export const insertRegular = async () => {
+  const newRegular: RegularTransfer = {
+    amount: 0,
+    ratio: 0,
+    percentage: false,
+  };
+  try {
+    await axios.post('api/regular/insert', newRegular);
+    console.log('Success to insert RegularTransfer')
+  } catch (error) {
+    console.log('Failed to update RegularTransfer');
+    // TODO: ログインし直し
   }
 };
