ce403008b3d5e35d1b0bdf01590db05b76876b4f
ryota8128 <ryota2339@icloud.com>
2023-07-29 16:41:09 +0900

    feat: api account insert

---
+++ src/pages/api/account/insert.ts
@@ -0,0 +1,29 @@
+import { isTokenExpired } from '@/lib/ JwtUtils';
+import axios from 'axios';
+import { error } from 'console';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getToken } from 'next-auth/jwt';
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const user = await getToken({ req });
+  const token = user?.accessToken;
+
+  if (!token || isTokenExpired(token)) {
+    return res.status(401).json({ error: '認証が必要です' });
+  }
+
+  let apiRes;
+  try {
+    apiRes = await axios.post(`${apiUrl}/api/account`, req.body, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      withCredentials: true,
+    });
+    return res.status(apiRes.status).json(apiRes.data);
+  } catch (error) {
+    return res.status(401).json({ error: '認証に失敗しました' });
+  }
+}
