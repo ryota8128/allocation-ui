4e776df3e516091ab48a13128d9fd4eea793c641
ryota8128 <ryota2339@icloud.com>
2023-08-03 00:40:56 +0900

    feat: 最適化アルゴリズム実装

--- src/lib/optimize.ts
+++ src/lib/optimize.ts
@@ -12,6 +12,12 @@ type Summary = {
   [key: number]: number;
 };
 
+type TransferSummary = {
+  from: number;
+  to: number;
+  amount: number;
+};
+
 export default function optimize(
   regularList: RegularTransfer[],
   temporaryList: TemporaryTransfer[]
@@ -22,7 +28,8 @@ export default function optimize(
   validate(rtList);
   // サマリーにまとめる
   const summary: Summary = integrate(rtList);
-  console.log(summary);
+  const result: TransferSummary[] = calcTransfers(summary);
+  console.log(result);
 }
 
 function buildRtTransfer(
@@ -85,4 +92,39 @@ function integrate(rtList: RtTransfer[]) {
   });
 
   return transferSummary;
+}
+
+function calcTransfers(accounts: { [key: number]: number }): TransferSummary[] {
+  const transfers: TransferSummary[] = [];
+
+  const positiveAccounts: number[] = Object.keys(accounts)
+    .map(Number)
+    .filter((id) => accounts[id] > 0)
+    .sort((a, b) => accounts[a] - accounts[b]);
+
+  const negativeAccounts: number[] = Object.keys(accounts)
+    .map(Number)
+    .filter((id) => accounts[id] < 0)
+    .sort((a, b) => accounts[b] - accounts[a]);
+
+  while (positiveAccounts.length > 0 && negativeAccounts.length > 0) {
+    const fromAccount = negativeAccounts[0];
+    const toAccount = positiveAccounts[0];
+
+    const amount = Math.min(Math.abs(accounts[fromAccount]), accounts[toAccount]);
+
+    accounts[fromAccount] += amount;
+    accounts[toAccount] -= amount;
+
+    transfers.push({ from: fromAccount, to: toAccount, amount });
+
+    if (accounts[fromAccount] === 0) {
+      negativeAccounts.shift();
+    }
+    if (accounts[toAccount] === 0) {
+      positiveAccounts.shift();
+    }
+  }
+
+  return transfers;
 }
