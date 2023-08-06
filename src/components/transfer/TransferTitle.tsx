337ea69d97239d14763fde6de76b53733e779f1f
ryota8128 <ryota2339@icloud.com>
2023-08-07 00:23:29 +0900

    feat: transfer更新機能

--- src/components/transfer/TransferTitle.tsx
+++ src/components/transfer/TransferTitle.tsx
@@ -1,9 +1,10 @@
+import Transfer from '@/types/Transfer';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { CSSProperties, useEffect, useRef, useState } from 'react';
 import { LiaEditSolid } from 'react-icons/lia';
 import { RiDeleteBin5Line } from 'react-icons/ri';
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import { Button, Input, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 
 interface Props {
   title: string;
@@ -20,6 +21,8 @@ const TransferTitle: React.FC<Props> = ({ title, transferId, setErrorMsg }) => {
   const [isHoverEdit, setIsHoverEdit] = useState(false);
   const [isHoverDelete, setIsHoverDelete] = useState(false);
   const [modal, setModal] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [editingTitle, setEditingTitle] = useState(title);
 
   const toggle = () => setModal(!modal);
 
@@ -70,10 +73,24 @@ const TransferTitle: React.FC<Props> = ({ title, transferId, setErrorMsg }) => {
     toggle();
   };
 
+  const handleClickUpdateTransferTitle = async () => {
+    try {
+      const newTransfer: Transfer = {
+        id: transferId,
+        title: editingTitle,
+      };
+      await axios.patch(`/api/transfer/update`, newTransfer);
+      console.log('Transferのタイトルを更新しました');
+      router.reload();
+    } catch (error) {
+      setErrorMsg('タイトルの更新に失敗しました');
+    }
+  };
+
   return (
     <>
       <Modal isOpen={modal} toggle={toggle} size="lg" backdrop={true} fade>
-        <ModalHeader toggle={toggle} close>
+        <ModalHeader toggle={toggle} close={<></>}>
           振替ページ削除
         </ModalHeader>
         <ModalBody>
@@ -96,22 +113,43 @@ const TransferTitle: React.FC<Props> = ({ title, transferId, setErrorMsg }) => {
         onMouseOut={() => handleMouseOut('div')}
         style={{ display: 'flex', alignItems: 'center' }}
       >
-        <h1>{title}</h1>
-        {isHover && (
+        {editing ? (
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Input
+              onChange={(e) => setEditingTitle(e.target.value)}
+              value={editingTitle}
+              style={{ fontSize: 36, height: 50 }}
+            />
+            <Button
+              color="primary"
+              outline
+              onClick={handleClickUpdateTransferTitle}
+              style={{ whiteSpace: 'nowrap', height: 50, marginBottom: 20, marginLeft: 40 }}
+            >
+              確定
+            </Button>
+          </div>
+        ) : (
           <>
-            <LiaEditSolid
-              size={30}
-              style={isHoverEdit ? { ...editIconStyle, ...hoverStyle } : editIconStyle}
-              onMouseOver={() => handleMouseOver('edit')}
-              onMouseOut={() => handleMouseOut('edit')}
-            />
-            <RiDeleteBin5Line
-              size={30}
-              style={isHoverDelete ? { ...deleteIconStyle, ...hoverStyle } : deleteIconStyle}
-              onMouseOver={() => handleMouseOver('delete')}
-              onMouseOut={() => handleMouseOut('delete')}
-              onClick={toggle}
-            />
+            <h1>{title}</h1>
+            {isHover && (
+              <>
+                <LiaEditSolid
+                  size={30}
+                  style={isHoverEdit ? { ...editIconStyle, ...hoverStyle } : editIconStyle}
+                  onMouseOver={() => handleMouseOver('edit')}
+                  onMouseOut={() => handleMouseOut('edit')}
+                  onClick={() => setEditing(true)}
+                />
+                <RiDeleteBin5Line
+                  size={30}
+                  style={isHoverDelete ? { ...deleteIconStyle, ...hoverStyle } : deleteIconStyle}
+                  onMouseOver={() => handleMouseOver('delete')}
+                  onMouseOut={() => handleMouseOut('delete')}
+                  onClick={toggle}
+                />
+              </>
+            )}
           </>
         )}
       </div>
